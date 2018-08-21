b9e31cf8a1395234886e641f0c9400741c94efb8
Dustin Starke and Michael Harmon <michael@>
2018-08-21 14:28:20 -0700

    include update button functionality and address type

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,21 @@ function Contact(first, last) {
   this.lastName = last;
   this.addresses = [];
 }
-function Address(street, city, state, zip) {
+function Address(street, city, state, zip, type) {
+  this.type = type;
   this.street = street;
   this.city = city;
   this.state = state;
   this.zip = zip;
+
+
+  // this.type = type; To add multiple addresses
 }
   Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
   }
   Address.prototype.fullAddress = function() {
-    return this.street + ", " + this.city + ", " + this.state + " " + this.zip;
+    return this.type + ": " + this.street + ", " + this.city + ", " + this.state + " " + this.zip;
   }
 // user interface logic
 function resetFields() {
@@ -26,12 +30,78 @@ function resetFields() {
   $("input.new-city").val("");
   $("input.new-state").val("");
   $("input.new-zip").val("");
+  $("#home").prop("checked", true);
 }
+
+function appendNewContact(contact) {
+  $("ul#contacts").append("<li><span class='contact'>" + contact.fullName() + "</span></li>");
+}
+
+function showContactDetails(contact) {
+  $("#show-contact").show();
+  $("#show-contact h2").text(contact.fullName());
+  $(".first-name").text(contact.firstName);
+  $(".last-name").text(contact.lastName);
+  $("ul#addresses").text("");
+  contact.addresses.forEach(function(address) {
+    $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+    console.log(contact.addresses);
+  });
+}
+
+function showEditForm(contact){
+  var address = contact.addresses[0]
+  $("form#new-contact").hide();
+  $("form#edit-contact").show();
+  $("input#update-first-name").val(contact.firstName);
+  $("input#update-last-name").val(contact.lastName);
+  var type = address.type
+  console.log(type);
+  if (type == "Home"){
+    $("#edit-home").prop("checked", true);
+  } else if (type == "Work"){
+    $("#edit-work").prop("checked", true);
+  } else if (type == "POBox"){
+    $("#edit-PObox").prop("checked", true);
+  }
+  $("input.update-street").val(address.street);
+  $("input.update-city").val(address.city);
+  $("input.update-state").val(address.state);
+  $("input.update-zip").val(address.zip)
+  // hide add contact form
+  // Show edit contact form
+  // populate edit contact form with contact info
+}
+ function updateContact(contact) {
+   var address = contact.addresses[0]
+  contact.firstName = $("input#update-first-name").val()
+  contact.lastName = $("input#update-last-name").val()
+  address.type = $("input:radio[name=edit-type]:checked").val();
+  address.street = $("input.update-street").val()
+  address.city = $("input.update-city").val()
+  address.state = $("input.update-state").val()
+  address.zip = $("input.update-zip").val()
+  $("form#new-contact").show();
+  $("form#edit-contact").hide();
+ }
+
+
 $(document).ready(function() {
 
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address">' +
-                                 '<div class="form-group">' +
+                                '<div class="btn-group" data-toggle="buttons">'+
+                                '<label class="btn btn-primary active">'+
+                                '<input type="radio" name="type" id="home" value="Home" autocomplete="off" checked> Home'+
+                                '</label>'+
+                                '<label class="btn btn-primary">'+
+                                '<input type="radio" name="type" id="work" value="Work" autocomplete="off"> Work'+
+                                '</label>'+
+                                '<label class="btn btn-primary">'+
+                                '<input type="radio" name="type" id="PObox" value="POBox" autocomplete="off"> PObox'+
+                                '</label>'+
+                                '</div>'+
+                                '<div class="form-group">' +
                                    '<label for="new-street">Street</label>' +
                                    '<input type="text" class="form-control new-street">' +
                                  '</div>' +
@@ -56,29 +126,35 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    contactArray.push(newContact);
 
     $(".new-address").each(function() {
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
       var inputtedZip =$(this).find("input.new-zip").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedZip)
+      var inputtedType = $("input:radio[name=type]:checked").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedZip, inputtedType)
       newContact.addresses.push(newAddress)
     });
+    contactArray.push(newContact);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    appendNewContact(newContact);
 
     $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
+      showContactDetails(newContact);
     });
+    $("#edit-button").click(function(){
+      showEditForm(newContact);
+      console.log("test");
+      // inside click function perform editContact function
+    })
+    $("form#edit-contact").submit(function(event) {
+      event.preventDefault();
+      updateContact(newContact);
+      console.log("test2");
+    })
+    // If there are multiple addresses, edit form should expand to edit other addresses
+
     resetFields()
   });
 });
